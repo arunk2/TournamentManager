f71377906a74711f9f15f9f2bb16b28963eefc12
arunk2 <emailarunkumar@gmail.com>
2017-11-03 16:57:46 +0530

    Ref #3: Added mysql DDL stmts

---
+++ DBModel/DDL.sql
@@ -0,0 +1,11 @@
+CREATE TABLE club (
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name varchar(50) NOT NULL,
+abb varchar(10) ,
+addr_line_1 varchar(200) ,
+addr_line_2 varchar(200) ,
+city varchar(50) ,
+state varchar(50) ,
+country varchar(50) 
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
